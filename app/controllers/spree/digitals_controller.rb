2cb8605aa25c83ef86789ace2c399ae5a8d09c22
Evgeny Sugakov <shiroginne@gmail.com>
2017-08-03 18:08:23 +0200

    Add 'drm' flag for Spree::Digitals. Fix syntax.

--- app/controllers/spree/digitals_controller.rb
+++ app/controllers/spree/digitals_controller.rb
@@ -12,7 +12,7 @@ module Spree
             if Paperclip::Attachment.default_options[:storage] == :s3
               redirect_to attachment.expiring_url(Spree::DigitalConfiguration[:s3_expiration_seconds]) and return
             else
-              send_file attachment.path, :filename => attachment.original_filename, :type => attachment.content_type and return
+              send_file attachment.path, filename: attachment.original_filename, type: attachment.content_type and return
             end
           end
         else
