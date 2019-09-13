4189ef0d85a6aca9230344dd9c87fc8d831e4e11
Alessandro Desantis <desa.alessandro@gmail.com>
2019-09-13 08:45:34 +0200

    Use dynamic attributes in factories
    Static attributes have been deprecated by FactoryBot and don't
    work properly anymore.

--- spec/factories/digital_factory.rb
+++ spec/factories/digital_factory.rb
@@ -1,8 +1,7 @@
 FactoryBot.define do
   factory :digital, :class => Spree::Digital do |f|
     f.variant { |p| p.association(:variant) }
-
-    attachment_content_type 'application/octet-stream'
-    attachment_file_name "#{SecureRandom.hex(5)}.epub"
+    f.attachment_content_type { 'application/octet-stream' }
+    f.attachment_file_name { "#{SecureRandom.hex(5)}.epub" }
   end
 end
