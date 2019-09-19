202be313d86ae00704da242bcc0d9259f5acb8f7
Alessandro Desantis <desa.alessandro@gmail.com>
2019-09-19 10:24:33 +0200

    Fix FactoryBot not finding definitions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,8 @@ require 'spree/testing_support/capybara_ext'
 
 Dir[File.join(File.dirname(__FILE__), "support/**/*.rb")].each { |f| require f }
 
+FactoryBot.find_definitions
+
 RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.raise_errors_for_deprecations!
