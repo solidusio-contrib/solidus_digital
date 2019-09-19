b7444f5c31a762434b0a689cc447e2f19e1a8b3f
Alessandro Desantis <desa.alessandro@gmail.com>
2019-09-19 11:05:10 +0200

    Test Admin::OrdersController in integration

--- spec/controllers/admin/orders_controller_spec.rb
+++ spec/controllers/admin/orders_controller_spec.rb
@@ -4,17 +4,26 @@ RSpec.describe Spree::Admin::OrdersController do
   context "with authorization" do
     stub_authorization!
 
+    let(:order) do
+      create(:completed_order_with_totals) do |o|
+        create(:digital, variant: o.line_items.first.variant)
+      end
+    end
+    let!(:digital_link) do
+      create(:digital_link, access_counter: 3, line_item: order.line_items.first)
+    end
+
     before do
       request.env["HTTP_REFERER"] = "http://localhost:3000"
     end
 
-    let(:order) { mock_model(Spree::Order, :complete? => true, :total => 100, :number => 'R123456789') }
-    before { allow(Spree::Order).to receive_messages :find_by_number! => order }
-
     context '#reset_digitals' do
       it 'should reset digitals for an order' do
-        expect(order).to receive(:reset_digital_links!)
-        get :reset_digitals, params: { id: order.number }
+        expect do
+          get :reset_digitals, params: { id: order.number }
+          digital_link.reload
+        end.to change(digital_link, :access_counter).to(0)
+
         expect(response).to redirect_to(spree.admin_order_path(order))
       end
     end
